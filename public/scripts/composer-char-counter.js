61796c2c4d1e8ac4256a7707b8120884a4aed7de
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-12-16 18:39:32 +0000

    Created a function to automatically scroll back up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,43 @@
-$(document).ready(function() {
-  // --- our code goes here ---
-  document.querySelector('#tweet-text').addEventListener('input', function(event){
-    let counter = $(this).parent().children().find('.counter');
-    let length = $(this).val().length;
-    let update = $(counter).val(140 - length)
-    update;
-   
-  // $( "#tweet-text" ).keyup(function(event) {
-  //   var tweet = event.target.value.length;
-  //   console.log(tweet);
-  //   $(".counter").text (140 - tweet);
-    if (update.val() < 0 ) {
-      $(".counter").css("color", "red");
-    } else {
-      $(".counter").css("color", "black");
-    }
+const toggleBackToTopBtn = () => {
+  if ($(window).scrollTop() > 0) {
+    $(".back-to-top")
+      .show()
+      .fadeIn("slow");
+  } else {
+    $(".back-to-top")
+      .hide()
+      .fadeOut("slow");
+  }
+};
+
+
+$(document).ready(function () {
+  // --- our code goes here --- 
+  document
+    .querySelector("#tweet-text")
+    .addEventListener("input", function (event) {
+      let counter = $(this).parent().children().find(".counter");
+      let length = $(this).val().length;
+      let update = $(counter).val(140 - length);
+      update;
+
+      if (update.val() < 0) {
+        $(".counter").css("color", "red");
+      } else {
+        $(".counter").css("color", "black");
+      }
+    });
+
+  // hide back to top button at initial render
+  $(".back-to-top").hide();
+  // call toggleBackTpTopBtn on page scroll
+  $(window).scroll(toggleBackToTopBtn);
+  // clicking on back to top
+  $(".back-to-top").on("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    toggleBackToTopBtn();
   });
-// })
 });
